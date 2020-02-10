7e75faa78507cf146f7919c34bf6bdc7507b1a90
Anastasia.Tab <anastasia.tab@gmail.com>
2020-02-10 12:36:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
